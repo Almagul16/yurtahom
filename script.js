af451eb1f8364d4c8f81cb74ecf536d1071a9511
Алмагуль Узыканова <almaguluzykanova@MacBook-Air-Almagul.local>
2023-11-18 18:23:15 +0600

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const back = document.querySelector('#back');
 const next = document.querySelector('#next');
 
-const photos = ["slaidone.avif", "slaidtwo.avif", "slaidthree.avif"];
+const photos = ["banner2.jpg", "banner.jpg", "banner1.jpg"];
 
 let i = 0;
 
@@ -19,3 +19,14 @@ let i = 0;
     }
     document.querySelector('#pictures').src = photos[i];
  } )
+
+
+ const searchInput = document.querySelector("#search");
+ const divs = document.querySelectorAll (".one");
+ 
+ searchInput.addEventListener ("keyup",function(event){
+     const word = event.target.value.toLowerCase ();
+     divs.forEach(item => {
+         item.querySelector("p").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display = "none");
+     })
+ })
